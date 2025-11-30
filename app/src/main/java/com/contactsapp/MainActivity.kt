048bbcdb5d16618605c7f8f4f1c370c5f9f37fcf
byt3buster <byt3buster@gmail.com>
2025-11-30 16:08:26 +0100

    feat(ui): add FloatingActionButton

--- app/src/main/java/com/contactsapp/MainActivity.kt
+++ app/src/main/java/com/contactsapp/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.contactsapp.components.ScrollContent
 import com.contactsapp.components.TopBar
 import com.contactsapp.ui.theme.ContactsAppTheme
@@ -22,7 +31,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ContactsAppTheme {
-                Scaffold(topBar = { TopBar() }) { innerPadding ->
+                Scaffold(
+                    topBar = { TopBar() },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {},
+                            shape = CircleShape,
+                            containerColor = Color(0xFF00B2FF),
+                            contentColor = Color.White,
+                        ){
+                                Icon(
+                                    imageVector = Icons.Filled.Add,
+                                    contentDescription = "Add new contact"
+                                )
+
+                        }
+
+                    }) { innerPadding ->
                     Column(
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally,
--- app/src/main/java/com/contactsapp/components/AppComponents.kt
+++ app/src/main/java/com/contactsapp/components/AppComponents.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -43,7 +45,7 @@ fun TopBar() {
                     contentDescription = "More actions"
                 )
             }
-        },
+        }
     )
 }
 
